fc1ec318e57284b66997d11c04d2535e160a9740
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-14 16:00:53 -0400

    done through 2.3

--- handlers/clientHandlers.js
+++ handlers/clientHandlers.js
@@ -1,4 +1,40 @@
-const { v4: uuidv4 } = require('uuid');
-const { clients } = require('../data/clients');
+const { v4: uuidv4 } = require("uuid");
+const { clients } = require("../data/clients");
 
-// write your handlers here...
+function findClient(id) {
+  return clients.find((client) => client.id == id);
+}
+function handleClients(req, res) {
+  res.status(200).json({ clients: clients });
+}
+function handleClient(req, res, next) {
+  const clientID = req.params.id;
+  const client = findClient(clientID);
+  if (client) {
+    res.status(200).json({ status: 200, client });
+  } else next();
+}
+function handle404(req, res) {
+  res.status(404).json({ status: 404 });
+}
+
+function findClientEmail(id) {
+  return clients.find((client) => client.email == id);
+}
+
+function createNewGuy(req, res) {
+  const clientEmail = req.body.email;
+  const client = findClientEmail(clientEmail);
+  const newclient = req.body;
+  if (client) {
+    console.log("we have an email");
+    res.status(400).send("we already have this email on file");
+  } else {
+    console.log("can be added");
+    clients.push(newclient);
+    console.log(clients);
+    res.status(200).send("new homey added to the little friends");
+  }
+}
+
+module.exports = { handleClients, createNewGuy, handleClient, handle404 };
--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-'use strict';
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const {
+  handleClients,
+  handleClient,
+  handle404,
+  createNewGuy,
+} = require("./handlers/clientHandlers.js");
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-
   // endpoints
-
+  .get("/clients", handleClients)
+  .get("/clients/:id", handleClient)
+  .get("/*", handle404)
+  .post("/clients", createNewGuy)
   .listen(8000, () => console.log(`Listening on port 8000`));
